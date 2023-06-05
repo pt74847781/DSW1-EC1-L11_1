0ce0323b79c56d0302d1d55b6e5b4b9d2c817112
pt74847781 <ariel_sc_2001@hotmail.com>
2023-06-05 13:41:48 -0500

    octavocommit

--- src/main/java/pe/edu/idat/DSW1EC1L11_1/WebServiceConfig.java
+++ src/main/java/pe/edu/idat/DSW1EC1L11_1/WebServiceConfig.java
@@ -27,7 +27,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
 		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
 		wsdl11Definition.setPortTypeName("CountriesPort");
-		wsdl11Definition.setLocationUri("/ws");
+		wsdl11Definition.setLocationUri("/wsec1e1");
 		wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
 		wsdl11Definition.setSchema(countriesSchema);
 		return wsdl11Definition;
