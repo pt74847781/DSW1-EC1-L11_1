f41f8538268dd7cff67ffb066489927da856272b
pt74847781 <ariel_sc_2001@hotmail.com>
2023-06-05 13:00:54 -0500

    first commit

---
+++ src/main/java/pe/edu/idat/DSW1EC1L11_1/Dsw1Ec1L111Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.DSW1EC1L11_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1Ec1L111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1Ec1L111Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/DSW1EC1L11_1/Dsw1Ec1L111ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.DSW1EC1L11_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1Ec1L111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
